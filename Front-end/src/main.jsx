1c40ba4707551b9ef590e016fef776b58338bd77
salvador <salvador.cahuana3@gmail.com>
2024-05-23 13:13:16 -0300

    clientid

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,29 +1,28 @@
 //import './index.css'
-import React from 'react'
-import App from './App.jsx'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter} from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import {store, persistor} from './Redux/store.js'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import axios from 'axios'
+import React from "react";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./Redux/store.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
 
-//axios.defaults.baseURL = 'http://localhost:3001';
+//axios.defaults.baseURL = "http://localhost:3001";
 
 axios.defaults.baseURL = 'https://nonflix-backend-production.up.railway.app';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <GoogleOAuthProvider clientId="377367932904-c79vdhrt2cgf1uq0t2p3qh078kdtk9od.apps.googleusercontent.com">
-          <App />
-        </GoogleOAuthProvider>
-     </React.StrictMode>
-    </BrowserRouter>
+      <BrowserRouter>
+        <React.StrictMode>
+          <GoogleOAuthProvider clientId="176801974472-oi6mio4qqkocbjmbq431rnd55249j9a5.apps.googleusercontent.com">
+            <App />
+          </GoogleOAuthProvider>
+        </React.StrictMode>
+      </BrowserRouter>
     </PersistGate>
   </Provider>
-)
+);
