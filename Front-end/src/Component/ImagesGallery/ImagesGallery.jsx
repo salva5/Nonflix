991e52cfa4899cf55d3b3675e5b40e09d64db5a6
salvador <salvador.cahuana3@gmail.com>
2024-12-12 00:18:19 -0300

    refactor: modificar estilos de swiper

--- Front-end/src/Component/ImagesGallery/ImagesGallery.jsx
+++ Front-end/src/Component/ImagesGallery/ImagesGallery.jsx
@@ -1,18 +1,13 @@
 import {register} from 'swiper/element/bundle'
 register();
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
+import 'swiper/css/bundle'
 import {Swiper, SwiperSlide} from 'swiper/react';
-import { Autoplay, Navigation} from "swiper/modules"
+import { Autoplay,Pagination,EffectFade} from "swiper/modules"
 import style from './ImagesGallery.module.css'
 import LogInMenu from '../LogInMenu/LogInMenu';
 
 export default function ImagesGallery() {
-
-
   const data = [
     {id: '1', image: 'https://res.cloudinary.com/dy8pp1s5f/image/upload/v1696405506/Nonfilx_admins/cqxtgsws0klfjibdnxg7.jpg'},
     {id: '2', image: 'https://res.cloudinary.com/dy8pp1s5f/image/upload/v1696853069/Nonfilx_admins/sk5hhut8b9kg88hstq2g.jpg'},
@@ -21,26 +16,27 @@ export default function ImagesGallery() {
     {id: '5', image: 'https://res.cloudinary.com/dy8pp1s5f/image/upload/v1696316301/Nonfilx_admins/vzzi8sdqeyemiiwti7aa.jpg'},
     {id: '6', image: 'https://res.cloudinary.com/dy8pp1s5f/image/upload/v1696829100/Nonfilx_admins/tnesyzyccxoceszcr0df.webp'},
   ]
-
   return (
     <div className={style.swiper}>
       <LogInMenu />
       <Swiper
-        modules={[Autoplay, Navigation]}
-        slidesPerView={1}
-        pagination={{clickable: true}}
-        autoplay
+        modules={[Autoplay,Pagination,EffectFade]}
+        pagination={{dynamicBullets:true}}
+        autoplay={{disableOnInteraction:false}}
+        effect={"fade"}
+        loop
       >
-        {data.map((item)=> (
-          <SwiperSlide key={item.id} className={style.slideItem}>
-            <img
-              className={style.img}
-              src={item.image}
-              alt='Slider'
-              
-            />
-          </SwiperSlide>
-        ))}
+        {
+          data.map((item)=> (
+            <SwiperSlide  key={item.id} className={style.slideItem}>
+              <img
+                className={style.img}
+                src={item.image}
+                alt='Slider'
+              />
+            </SwiperSlide>
+          ))
+        }
       </Swiper>
 
 
