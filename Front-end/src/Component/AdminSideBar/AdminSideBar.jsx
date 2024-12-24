0b6cd47d7f1db6d1f8c842a933b137da864b40d3
salvador <salvador.cahuana3@gmail.com>
2024-12-24 19:24:45 -0300

    style(sidebarAdmin): motrar sidebar de forma dinamica por medio de un boton

--- Front-end/src/Component/AdminSideBar/AdminSideBar.jsx
+++ Front-end/src/Component/AdminSideBar/AdminSideBar.jsx
@@ -1,74 +1,91 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../../assets/NONFLIX-LOGO.png";
-import style from "./AdminSideBar.module.css";
+import styles from "./AdminSideBar.module.css";
 import homeIcon from "../../assets/round_home_white_24dp.png";
 import profileIcon from "../../assets/round_person_outline_white_24dp.png";
 import logOutIcon from "../../assets/round_logout_white_24dp.png";
 import ordersIcon from "../../assets/receipt_long_white_24dp.svg";
 import graphicsIcon from "../../assets/insights_white_24dp.svg";
-import shoppingCartIcon from "../../assets/round_shopping_cart_white_24dp.png";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import {
-  clearUserData,
-  getGenres,
+   clearUserData,
+   getGenres,
 } from "../../Redux/actions/actions";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import movieIcon from "../../assets/movie_white_24dp.svg";
 import usersIcon from "../../assets/group_white_24dp.svg";
 
 const AdminSideBar = () => {
-    
-  const dispatch = useDispatch();
-  const handleLogOut = () => {
-    dispatch(clearUserData());
-  };
+   const dispatch = useDispatch();
+   const handleLogOut = () => {
+      dispatch(clearUserData());
+   };
 
-  useEffect(() => {
-    dispatch(getGenres());
-  }, []);
+   useEffect(() => {
+      dispatch(getGenres());
+   }, []);
 
-  return (
-    <div className={style.main}>
-      <Link to="/Home">
-        <div className={style.logo}>
-          <img className="sidebar_image" src={logo} />
-        </div>
-      </Link>
-      <div className={style.menu}>
-        <h3>Menu</h3>
-        <Link to="/Dashboard">
-          <img src={movieIcon} />
-          <div>Movies</div>
-        </Link>
-        <Link to="/users" id="Users">
-          <img src={usersIcon} />
-          <div className={style.users}>Users</div>
-        </Link>
-        
-        <Link to="/orders">
-          <img src={ordersIcon} />
-          <div>Orders</div>
-        </Link>
-        <Link to="/graphics">
-          <img src={graphicsIcon} />
-          <div>Graphics</div>
-        </Link>
-        <Link to="/Home">
-          <img src={homeIcon} />
-          <div>Nonflix HOME</div>
-        </Link>
-
-        <h3>General</h3>
-        <Link to="/Profile">
-          <img src={profileIcon} />
-          <div>Profile</div>
-        </Link>
-        <Link to="/">
-          <img src={logOutIcon} />
-          <div onClick={() => handleLogOut()}>Logout</div>
-        </Link>
-      </div>
-    </div>
-  );
+   return (
+      <>
+         <Navbar expand="false" className=" position-absolute navbar-dark">
+            <Container >
+               <div className="d-flex align-items-center">
+                  <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} className={styles.toggle}/>  
+                  <NavLink to="/Home">
+                     <div className={styles.logo}>
+                        <img src={logo} />
+                     </div>
+                  </NavLink>
+               </div>
+               <Navbar.Offcanvas 
+                  id={`offcanvasNavbar-expand-${false}`}
+                  aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+                  placement="start"
+                  className={styles.ofCanvas}
+               >  
+                  <NavLink to="/Home">
+                     <div className={styles.logo}>
+                        <img src={logo} />
+                     </div>
+                  </NavLink>
+                  <div className={styles.menu}>
+                     <h3>Menu</h3>
+                     <NavLink to="/Dashboard">
+                        <span><img src={movieIcon}/>Movies</span>
+                     </NavLink>
+                     <NavLink to="/users" id="Users">
+                        <span><img src={usersIcon}/>Users</span>
+                     </NavLink>
+                     <NavLink to="/orders">
+                        <span><img src={ordersIcon}/>Orders</span>
+                     </NavLink>
+                     <NavLink to="/graphics">
+                        <span><img src={graphicsIcon} />Graphics</span>
+                     </NavLink>
+                     <NavLink to="/Home">
+                        <span><img src={homeIcon} />Nonflix HOME</span>
+                     </NavLink>
+                  </div>
+                  <div className={styles.general}>
+                     <h3>General</h3>
+                     <NavLink to="/Profile">
+                        <span><img src={profileIcon} />Profile</span>
+                     </NavLink>
+                     <NavLink 
+                        to="/"
+                        onClick={() => handleLogOut()}
+                     >
+                        <span><img src={logOutIcon} />Logout</span>
+                     </NavLink>
+                  </div>
+               </Navbar.Offcanvas>
+            </Container>
+         </Navbar>
+         
+      </>
+   );
 };
 export default AdminSideBar;
+
