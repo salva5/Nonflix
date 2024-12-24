7ff52f6b4a0aca9e381f793281d803a40bb2daf9
salvador <salvador.cahuana3@gmail.com>
2024-12-24 20:07:37 -0300

    style(sidebar): mostrar y ocultar sidebar dinamicamente

--- Front-end/src/Component/SideBar/SideBar.jsx
+++ Front-end/src/Component/SideBar/SideBar.jsx
@@ -1,151 +1,182 @@
-import { Link, NavLink } from "react-router-dom";
+import styles from "./SideBar.module.css";
 import logo from "../../assets/NONFLIX-LOGO.png";
-import style from "./SideBar.module.css";
 import homeIcon from "../../assets/round_home_white_24dp.png";
 import profileIcon from "../../assets/round_person_outline_white_24dp.png";
 import logOutIcon from "../../assets/round_logout_white_24dp.png";
 import favoriteIcon from "../../assets/round_favorite_border_white_24dp.png";
 import shoppingCartIcon from "../../assets/round_shopping_cart_white_24dp.png";
 import adminIcon from "../../assets/admin_panel_settings_white_24dp.svg";
+import { NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import { useDispatch, useSelector } from "react-redux";
-import {
-  filterParameters,
-  setCurrentPage,
-  clearUserData,
-  getGenres,
-  resetCart,
-} from "../../Redux/actions/actions";
 import { useEffect, useState } from "react";
+import {
+   filterParameters,
+   setCurrentPage,
+   clearUserData,
+   getGenres,
+   resetCart,
+} from "../../Redux/actions/actions";
 
-const SideBar = () => {
-  const [selectedGenre, setSelectedGenre] = useState("");
-  const stateFilterParams = useSelector((state) => state.filterParameters);
-  const Cart = useSelector((state) => state.Cart);
-  const genres = useSelector((state) => state.genres);
-  const userData = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-  const userFirstName = userData.name.split(" ");
 
-  useEffect(() => {
-    dispatch(getGenres());
-  }, []);
+function SideBar() {
 
-  const handleCategoryClick = (event) => {
-    const copyFilterParameters = stateFilterParams;
-    copyFilterParameters[0] = event.target.id;
-    setSelectedGenre(event.target.id);
-    copyFilterParameters[3] = null;
-    dispatch(setCurrentPage(1));
-    dispatch(filterParameters(copyFilterParameters));
-  };
+   const [selectedGenre, setSelectedGenre] = useState("");
+   const stateFilterParams = useSelector((state) => state.filterParameters);
+   const Cart = useSelector((state) => state.Cart);
+   const genres = useSelector((state) => state.genres);
+   const userData = useSelector((state) => state.user);
+   const dispatch = useDispatch();
+   const userFirstName = userData.name.split(" ");
 
-  const handleSideBarClicks = () => {
-    setSelectedGenre("");
-    dispatch(setCurrentPage(1));
-  };
+   useEffect(() => {
+      dispatch(getGenres());
+   }, []);
 
-  const handleHomeClick = () => {
-    setSelectedGenre("Home");
-    const copyFilterParameters = stateFilterParams;
-    copyFilterParameters[0] = "Home";
-    copyFilterParameters[3] = null;
-    dispatch(filterParameters(copyFilterParameters));
-    dispatch(setCurrentPage(1));
-  };
-  const handleLogOut = () => {
-    dispatch(clearUserData());
-    dispatch(resetCart());
-  };
+   const handleCategoryClick = (event) => {
+      const copyFilterParameters = stateFilterParams;
+      copyFilterParameters[0] = event.target.id;
+      setSelectedGenre(event.target.id);
+      copyFilterParameters[3] = null;
+      dispatch(setCurrentPage(1));
+      dispatch(filterParameters(copyFilterParameters));
+   };
 
-  return (
-    <div className={style.main}>
-      <Link to="/Home">
-        <div className={style.logo}>
-          <img className="sidebar_image" src={logo} />
-        </div>
-      </Link>
-      <div className={style.menu}>
-        <h2>Hello {userFirstName[0]}!</h2>
-        <h3>Menu</h3>
-        <Link
-          to="/Home"
-          id="Home"
-          onClick={() => handleHomeClick()}
-          className={selectedGenre === "Home" ? style.selected : style.none}
-        >
-          <img src={homeIcon} />
-          Home
-        </Link>
-        <NavLink
-          to="/Favorites"
-          className={(navData) => (navData.isActive ? style.selected : "none")}
-          id="favorites"
-          onClick={() => handleSideBarClicks()}
-        >
-          <img src={favoriteIcon} />
-          Favorites
-        </NavLink>
-        <NavLink
-          to="/Cart"
-          onClick={(event) => handleSideBarClicks(event)}
-          className={(navData) => (navData.isActive ? style.selected : "none")}
-          id="cart"
-        >
-          <img src={shoppingCartIcon} />
-          Cart{" "}
-          {Cart.length ? (
-            <div className={style.circle}>{Cart.length}</div>
-          ) : (
-            <div></div>
-          )}
-        </NavLink>
+   const handleSideBarClicks = () => {
+      setSelectedGenre("");
+      dispatch(setCurrentPage(1));
+   };
 
-        <h3>Genre</h3>
-        {genres
-          ? genres.map((genre, index) => (
-              <Link to="/Home" key={index}>
-                <div
-                  className={
-                    selectedGenre === genre ? style.selected : style.none
-                  }
-                  key={index}
-                  onClick={(event) => handleCategoryClick(event)}
-                  id={genre}
-                >
-                  {genre}
-                </div>
-              </Link>
-            ))
-          : "Loading..."}
-        <h3>General</h3>
-        {userData.admin && (
-          <NavLink
-            to="/Dashboard"
-            className={(navData) =>
-              navData.isActive ? style.selected : "none"
-            }
-            id="profile"
-          >
-            <img src={adminIcon} />
-            Admin dashboard
-          </NavLink>
-        )}
-        <NavLink
-          to="/Profile"
-          className={(navData) => (navData.isActive ? style.selected : "none")}
-          id="profile"
-          onClick={() => handleSideBarClicks()}
-        >
-          <img src={profileIcon} />
-          Profile
-        </NavLink>
+   const handleHomeClick = () => {
+      setSelectedGenre("Home");
+      const copyFilterParameters = stateFilterParams;
+      copyFilterParameters[0] = "Home";
+      copyFilterParameters[3] = null;
+      dispatch(filterParameters(copyFilterParameters));
+      dispatch(setCurrentPage(1));
+   };
+   const handleLogOut = () => {
+      dispatch(clearUserData());
+      dispatch(resetCart());
+   };
 
-        <Link to="/">
-          <img src={logOutIcon} />
-          <div onClick={() => handleLogOut()}>Logout</div>
-        </Link>
-      </div>
-    </div>
-  );
-};
+   return (
+      <>
+         <Navbar expand="false" className=" position-absolute navbar-dark">
+            <Container >
+               <div className="d-flex align-items-center">
+                  <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} className={styles.toggle}/>  
+                  <NavLink to="/Home">
+                     <div className={styles.logo}>
+                        <img src={logo} />
+                     </div>
+                  </NavLink>
+               </div>
+               
+               <Navbar.Offcanvas 
+                  id={`offcanvasNavbar-expand-${false}`}
+                  aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+                  className={styles.navMain}
+               >  
+                  <NavLink to="/Home">
+                     <div className={styles.logo}>
+                        <img src={logo} />
+                     </div>
+                  </NavLink>
+                  <div className={styles.menu}>
+                     <h2>Hello {userFirstName[0]}!</h2>
+                     <div className={styles.views}>
+                        <h3>Menu</h3>
+                        <NavLink
+                           to="/Home"
+                           id="Home"
+                           onClick={() => handleHomeClick()}
+                           className={selectedGenre === "Home" ? styles.selected : styles.none}
+                        >
+                           <span><img src={homeIcon} />Home</span> 
+                           
+                        </NavLink>
+                        <NavLink
+                           to="/Favorites"
+                           className={(navData) => (navData.isActive ? styles.selected : "none")}
+                           id="favorites"
+                           onClick={() => handleSideBarClicks()}
+                        >
+                           <span ><img src={favoriteIcon}/>Favorites</span>
+                        </NavLink>
+                        <NavLink
+                           to="/Cart"
+                           onClick={(event) => handleSideBarClicks(event)}
+                           className={(navData) => (navData.isActive ? styles.selected : "none")}
+                           id="cart"
+                        >
+                           <span> 
+                              <img src={shoppingCartIcon} />
+                              Cart{" "}
+                              {
+                                 Cart.length 
+                                    ? (<div className={styles.circle}>{Cart.length}</div>)     
+                                    : (<div></div>)
+                              }
+                           </span>
+                        </NavLink>
+                     </div>
+                     <div className={styles.genres}>
+                        <h3>Genres</h3>
+                        {
+                           genres
+                              ? genres.map((genre, index) => (
+                                    <NavLink to="/Home" key={index}>
+                                       <span
+                                          className={
+                                          selectedGenre === genre ? styles.selected : styles.none
+                                          }
+                                          key={index}
+                                          onClick={(event) => handleCategoryClick(event)}
+                                          id={genre}
+                                       >
+                                          {genre}
+                                       </span>
+                                    </NavLink>
+                                 ))
+                              : "Loading..."
+                        }
+                     </div>
+                        
+                     <div className={styles.general}>
+                        <h3>General</h3>
+                        {
+                           userData.admin && (
+                              <NavLink
+                                 to="/Dashboard"
+                                 className={(navData) =>
+                                 navData.isActive ? styles.selected : "none"
+                                 }
+                                 id="profile"
+                              >
+                                 <span><img src={adminIcon} />Admin dashboard</span>
+                              </NavLink>
+                           )   
+                        }
+                        <NavLink
+                           to="/Profile"
+                           className={(navData) => (navData.isActive ? styles.selected : "none")}
+                           id="profile"
+                           onClick={() => handleSideBarClicks()}
+                        >
+                           <span><img src={profileIcon} />Profile</span>
+                        </NavLink>
+                        <NavLink to="/" onClick={() => handleLogOut()}>
+                           <span><img src={logOutIcon}/>Logout</span>
+                        </NavLink>
+                     </div>
+                  </div>
+               </Navbar.Offcanvas>
+            </Container>
+         </Navbar>
+      </>
+   );
+}
+
 export default SideBar;
