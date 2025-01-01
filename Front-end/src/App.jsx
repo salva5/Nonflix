537a66deb71cb62530a3aee989f0cef4c60b56d7
salvador <salvador.cahuana3@gmail.com>
2025-01-01 17:07:12 -0300

    refactor(routes): mapear las rutas se crea un array con todas las rutas y sus componentes a renderizar para que el codigo no se vea tan verboso

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,77 +1,81 @@
-//import { useState } from 'react'
-import React, { useEffect } from 'react';
-import './App.css'
-import  NonRegisteredHome  from "./views/NonRegisteredHome";
-import Register from './Component/Register/Register';
-import {Routes, Route, useNavigate} from "react-router-dom"
-import Login from './Component/Login/Login';
-import Home from './Component/Home/Home';
-import Detail from './Component/Detail/Detail';
+import React, { useEffect } from "react";
+import "./App.css";
+import NonRegisteredHome from "./views/NonRegisteredHome";
+import Register from "./Component/Register/Register";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./Component/Login/Login";
+import Home from "./Component/Home/Home";
+import Detail from "./Component/Detail/Detail";
 import NotFound from "./Component/NotFound/NotFound";
-import Cart from './Component/Cart/Cart';
-import PostMovie from "./Component/PostMovie/PostMovie"
-import Favs from './Component/Favorite/Favs';
-import Profile from './Component/Profile/Profile';
-import UploadImagesTohome from './Component/UploadImages/uploadImagesTohome';
-import ProtectedRoute from './Component/utils/ProtectedRoute';
-import { useSelector } from 'react-redux';
+import Cart from "./Component/Cart/Cart";
+import PostMovie from "./Component/PostMovie/PostMovie";
+import Favorites from "./Component/Favorite/Favorites"
+import Profile from "./Component/Profile/Profile";
+import UploadImagesTohome from "./Component/UploadImages/uploadImagesTohome";
+import ProtectedRoute from "./Component/utils/ProtectedRoute";
+import { useSelector } from "react-redux";
 import DashBoard from "./Component/DashBoard/DashBoard";
 import Users from "./Component/Users/Users";
-import Form from './Component/FormPutMovie/FormPutMovie';
-import Graphics from './Component/Graphics/Graphics';
-import Orders from './Component/Orders/Orders';
-import LayoutSideBar from './Component/utils/LayoutSideBar';
+import Form from "./Component/FormPutMovie/FormPutMovie";
+import Graphics from "./Component/Graphics/Graphics";
+import Orders from "./Component/Orders/Orders";
+import LayoutSideBar from "./Component/utils/LayoutSideBar";
+import LogInMenu from "./Component/LogInMenu/LogInMenu";
 
+const privateRoutes = [
+   { path: "/Home", element: <Home /> },
+   { path: "/Cart", element: <Cart /> },
+   { path: "/Detail/:id", element: <Detail /> },
+   { path: "/PostMovie", element: <PostMovie /> },
+   { path: "/uploadImages", element: <UploadImagesTohome /> },
+   { path: "/Favorites", element: <Favorites /> },
+   { path: "/profile", element: <Profile /> },
+   { path: "/dashboard", element: <DashBoard /> },
+   { path: "/users", element: <Users /> },
+   { path: "/uploadMovie/:id", element: <Form /> },
+   { path: "/graphics", element: <Graphics /> },
+   { path: "/orders", element: <Orders /> },
+];
 function App() {
-  const privateRoutes = [
-    { path: '/Home', element: <Home /> },
-    { path: '/Cart', element: <Cart /> },
-    { path: '/Detail/:id', element: <Detail /> },
-    { path: '/PostMovie', element: <PostMovie /> },
-    { path: '/uploadImages', element: <UploadImagesTohome /> },
-    { path: '/Favorites', element: <Favs /> },
-    { path: '/profile', element: <Profile /> },
-    { path: '/dashboard', element: <DashBoard /> },
-    { path: '/users', element: <Users /> },
-    { path: '/uploadMovie/:id', element: <Form /> },
-    { path: '/graphics', element: <Graphics /> },
-    { path: '/orders', element: <Orders /> },
-  ];
-  
-  const user = useSelector((state) => state.user);
-  // Redirect to home
-  const navigate = useNavigate();
-  
-  useEffect(() => {
-    if (user.token) {
-      if(!user.admin){
-        return navigate('/Home');
+   const user = useSelector((state) => state.user);
+   const navigate = useNavigate();
 
+   useEffect(() => {
+      if (user.token) {
+         if (!user.admin) {
+            return navigate("/Home");
+         }
       }
-    }
-  }, [user.token]);
+   }, [user.token]);
 
-  return (
-    <div>
-      <Routes>
-        {/*Publics Routes*/}
-        <Route exact path='/Login' element={<Login/>}/>
-        <Route exact path='/Register' element={<Register/>}/>
-        <Route exact path='/' element={<NonRegisteredHome/>}/>
-        
-        {/* Private Routes */}
-        <Route element={<ProtectedRoute canActivate={user.token}/>}>
-          {
-            privateRoutes.map(({path, element}) => {
-              return <Route path={path} element={<LayoutSideBar>{element}</LayoutSideBar>}/>
-            })
-          }
-          <Route path="*" element={<NotFound />} />
-        </Route>
+   return (
+      <>
+         {!user.token && <LogInMenu/>}
+         <Routes>
+            {/*Publics Routes*/}
+            <Route exact path="/Login" element={<Login />} />
+            <Route exact path="/Register" element={<Register />} />
+            <Route exact path="/" element={<NonRegisteredHome />} /> 
+            
 
-      </Routes>
-    </div>
-  )
+            {/* Private Routes */}
+            <Route element={<ProtectedRoute canActivate={user.token} />}>
+               {
+                  privateRoutes.map(({ path, element }) => {
+                     return (
+                        <Route
+                           key={path} 
+                           path={path}
+                           element={<LayoutSideBar>{element}</LayoutSideBar>}
+                        />
+                     );
+                  })
+               }
+               <Route path="*" element={<NotFound />} />
+            </Route>
+         </Routes>
+      </>
+   );
 }
 
-export default App
+export default App;
