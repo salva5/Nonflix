d8f3fdb4487805e9649b3cf9951896904f73cf79
salvador <salvador.cahuana3@gmail.com>
2025-01-01 17:03:50 -0300

    refactor(sidebar): crear layout para mostrar los diferentes sidebar desde un mismo componente dependiendo si es o no admin

--- Front-end/src/Component/utils/LayoutSideBar.jsx
+++ Front-end/src/Component/utils/LayoutSideBar.jsx
@@ -3,16 +3,16 @@ import SideBar from "../SideBar/SideBar"
 import AdminSideBar from "../AdminSideBar/AdminSideBar"
 
 const LayoutSideBar = ({children}) => {
-  const user = useSelector((state) => state.user)
+   const user = useSelector((state) => state.user)
 
-  return (
-    <>
-      {
-        user.admin ? <AdminSideBar/> : <SideBar/>
-      }
-      {children}
-    </>
-  )
+   return (
+      <>
+         {
+            user.admin ? <AdminSideBar/> : <SideBar/>
+         }
+         {children}
+      </>
+   )
 }
 
 export default LayoutSideBar
