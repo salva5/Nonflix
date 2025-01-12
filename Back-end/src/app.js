6367e3cdbe2ed217643a7af59815876bc6ae3ccd
salvador <salvador.cahuana3@gmail.com>
2025-01-12 16:31:07 -0300

    feat(cors) : cambiar url para acceso a servidor

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://nonflix-u4z213hul-salvadors-projects-7b4c0e2a.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
