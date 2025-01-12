23668c647bf8eb32d4d6b1ece69129482e9c8dc3
salvador <salvador.cahuana3@gmail.com>
2025-01-12 18:34:35 -0300

    fix(db): solucionar error de tipo de dato de url de sequelize

--- Back-end/src/db.js
+++ Back-end/src/db.js
@@ -7,7 +7,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DB_DEPLOY } = process.e
 
 const sequelize = new Sequelize(
    /* `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, */
-   DB_DEPLOY,
+   `${DB_DEPLOY}`,
+   
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
